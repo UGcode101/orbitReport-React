c99e4d5029ee9cb2e594b29b2f799c3563a974df
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-03-10 21:06:01 -0500

    confirm banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
+import Banner from "./components/Banner";
 
 function App() {
-  return (
-    <div>
-      <Buttons />
-      <Table />
-    </div>
-  );
+   return (
+      <div>
+         <Banner />
+         <Buttons />
+         <Table />
+      </div>
+   );
 }
 
 export default App;
